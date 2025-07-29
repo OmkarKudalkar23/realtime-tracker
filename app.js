70010635e5b14ed8e43e706cf29de1fb0bf11d23
OmkarKudalkar23 <omkarkudalkar2016@gmail.com>
2025-07-29 23:09:36 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const http = require("http");
+const path = require('path');
+const socketio = require("socket.io");
+
+const server = http.createServer(app);
+
+const io = socketio(server);
+
+app.set("view engine" ,"ejs");
+app.use(express.json());
+app.use(express.static(path.join(__dirname,"public")));
+
+io.on("connection" ,(socket) =>{
+   
+     socket.on("send-location",(data) =>{
+            io.emit("recv-location",  {id: socket.id,...data});
+     })
+     socket.on("disconnect",()=>{
+          io.emit("user-disconnexted", socket.id);
+     })
+
+
+     console.log("A new connection with id",socket.id);
+})
+
+app.get("/",(req,res) =>{
+     res.render("index");
+})
+server.listen(3000, '0.0.0.0');
